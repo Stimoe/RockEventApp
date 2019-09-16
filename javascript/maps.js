a27a87cc4af6f3ee8e1ee20c2937fd42802f472c
Michelle Santiago <msantiago2222@yahoo.com>
2019-09-16 14:18:10 -0700

    fixing conflict

--- javascript/maps.js
+++ javascript/maps.js
@@ -2,24 +2,24 @@ var startLat
 var startLong
 
 
-      var cardHorizontalDiv = $('<div class="card horizontal">')
-      var cardImageDiv = $('<div class="card-image">')
-      cardImageDiv.append('<img>').attr('src', fakeResponse.data[i].img-url )
-      cardHorizontalDiv.append(cardImageDiv)
+var cardHorizontalDiv = $('<div class="card horizontal">')
+var cardImageDiv = $('<div class="card-image">')
+cardImageDiv.append('<img>').attr('src', fakeResponse.data[i].img - url)
+cardHorizontalDiv.append(cardImageDiv)
 
-      var cardStackedDiv = $('<div class="card-stacked">')
-      var cardContentDiv = $('<div class"card-content">')
-      cardContentDiv.append("<h5>").attr("#", fakeResponse2.data[i].img-url)
-      cardContentDiv.append("<h1>").attr("#", fakeResponse2.data[i].img-url)
-      cardStackedDiv.append(cardContentDiv)
-      
-      var cardActionDiv = $('<div class="card-action">')
-      cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img-url )
-      cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img-url )
-      cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img-url )
-      cardActionDiv.append(cardActionImgDiv)
+var cardStackedDiv = $('<div class="card-stacked">')
+var cardContentDiv = $('<div class"card-content">')
+cardContentDiv.append("<h5>").attr("#", fakeResponse2.data[i].img - url)
+cardContentDiv.append("<h1>").attr("#", fakeResponse2.data[i].img - url)
+cardStackedDiv.append(cardContentDiv)
 
-    $(".main-card-container").append(cardHorizontalDiv, cardStackedDiv, cardActionDiv) 
+var cardActionDiv = $('<div class="card-action">')
+cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img - url)
+cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img - url)
+cardActionImgDiv.append('<a>').attr('#', fakeResponse.data[i].img - url)
+cardActionDiv.append(cardActionImgDiv)
+
+$(".main-card-container").append(cardHorizontalDiv, cardStackedDiv, cardActionDiv)
 
 //       });
 //     })
