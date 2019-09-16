0eb70c79e91d53747ca29ede01ff1f5384a71b28
Leah Munson <munsonleahr@gmail.com>
2019-09-16 10:44:20 -0700

    itunes api research

--- javascript/Itunes.js
+++ javascript/Itunes.js
@@ -8,3 +8,28 @@
 //     //   console.log(response)
 //   })}
 //   itunesInfo()
+
+
+//http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?
+
+
+// https://itunes.apple.com/search?term=jack+johnson
+
+//https://itunes.apple.com/search?parameterkeyvalue
+
+
+// var queryURL = http://itunes.apple.com/WebObjects/MZStoreServices.woa/wa/itmsSearch?lang=1&output=json&country=US&term=bandName&media=music
+
+// artistLinkUrl - what we want to pull out from the api results
+
+// use var bandNamesItunes in url to pull the bandNames from the results from the ticketmast events search
+
+// function itunesInfo() {
+// $.ajax({ 
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+// var bandNamesItunes = 
+//     //   console.log(response)
+//   })}
+//   itunesInfo()
