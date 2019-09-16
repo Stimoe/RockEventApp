0f25c8200a1170e31e74ce3c7d19c7997170283f
Stimoe <stimoe3@gmail.com>
2019-09-15 18:06:49 -0700

    got map working and with markers for events

--- javascript/ticketmaster.js
+++ javascript/ticketmaster.js
@@ -6,14 +6,125 @@
 
 // url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=Rock&apikey=2yfzA8sRxB5Z2ujcvJv5y6mV7gCVIKK4&postalCode=98101&startDateTime=2019-09-14T14:00:00Z&endDateTime=2019-09-25T14:00:00Z&latlong="+latlon,
 
-// var embed=[]
-// var genre=[]
-// var date=[]
-// var time=[]
+// url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=2yfzA8sRxB5Z2ujcvJv5y6mV7gCVIKK4&postalCode=98101&startDateTime=2019-09-14T14:00:00Z&endDateTime=2019-09-25T14:00:00Z&latlong="+latlon,
 
-// var longg=[]
-// var latss=[]
-// var venues=[]
-// var bandNames=[]
-// var images=[]
-// var locations=[];
+var embed=[]
+var genre=[]
+var date=[]
+var time=[]
+var event=[]
+var longg=[]
+var latss=[]
+var venues=[]
+var bandNames=[]
+var images=[]
+var locations=[];
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        var x = document.getElementById("location");
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    var x = document.getElementById("location");
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+
+    $.ajax({
+      type:"GET",
+      url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=Rock&apikey=2yfzA8sRxB5Z2ujcvJv5y6mV7gCVIKK4&postalCode=98101&startDateTime=2019-09-14T14:00:00Z&endDateTime=2019-09-25T14:00:00Z&latlong="+latlon,
+      async:true,
+      dataType: "json",
+      success: function(json) {
+                  console.log(json);
+                  var e = document.getElementById("events");
+                  e.innerHTML = json.page.totalElements + " events found.";
+                  showEvents(json);
+                  initMap(position, json);
+               },
+      error: function(xhr, status, err) {
+                  console.log(err);
+               }
+    });
+
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
+function showEvents(json) {
+  for(var i=0; i<json.page.totalElements; i++) {
+    // $("#events").append("<p>"+json._embedded.events[i]+"</p>");
+    
+  }
+}
+
+
+function initMap(position, json) {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+    zoom: 10
+  });
+  for(var i=0; i<json.page.totalElements; i++) {
+    addMarker(map, json._embedded.events[i]);
+    even=json._embedded.events[i]
+
+
+
+
+    event.push(even)
+    console.log(event)
+  }
+
+}
+
+function addMarker(map, event) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+    map: map
+  });
+  marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+//   console.log(marker);
+}
+
+
+
+
+getLocation();
+
+
+
+// function addMarker(map, event) {
+//     var marker = new google.maps.Marker({
+//       position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+//       map: map
+//     });
+//     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+//     // console.log(marker);
+//   }
+  
+  
+  
+  
+ 
