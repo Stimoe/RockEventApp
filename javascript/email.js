a54b363b4829b9e7d9104631b21c9e0d932a460d
Stimoe <stimoe3@gmail.com>
2019-09-16 16:53:01 -0700

    finished making the buttons for the venues drop a marker on the map

--- javascript/email.js
+++ javascript/email.js
@@ -1,4 +1,8 @@
+markerToMake={}
+venueForMarkers=[]
 
+  
+  
   function display(){
       
     $("#buttons-view").empty();
@@ -16,10 +20,52 @@
   $(document).on("click", ".venue-buttons", function(event) {
     console.log("clicked")
     event.preventDefault();
-    
+    var lats=$(this).attr("data-name2")
+    var lon=$(this).attr("data-name3")
+     var ven=$(this).attr("data-name")
 
     console.log($(this).attr("data-name1"))
     console.log($(this).attr("data-name2"))
     console.log($(this).attr("data-name3"))
+lat2= JSON.parse(lats)
+lon2= JSON.parse(lon)
   
+
+    // var myLatLng = {lat: lats, lng: lon};
+    markerToMake={lat: lat2, lng: lon2}
+  
+    //  var markers= JSON.parse(markerToMake)
+    //  console.log(markers)
+    console.log(markerToMake)
+    // markerToMake.lat.push(lats)
+    // markerToMake.lng.push(lon)
+    // venueForMarkers.push(ven)
+    // console.log(myLatLng)
+    
+    venueMarkers(markerToMake)
+      
+     
+    
+    
+
   })
+    
+
+    // addMarker()
+//      function addMarker(){
+//       console.log(myLatLng)
+//       var marker = new google.maps.Marker({
+
+       
+          
+        
+        
+//         position: myLatLng,
+//         map: map,
+//         title: 'Click to zoom'
+//       });
+
+//   }
+
+
+
