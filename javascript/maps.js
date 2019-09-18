490d0e1b124c14401eff0cace3d1505cc6f7e201
J-Anne Carlson <carlsonjc3@gmail.com>
2019-09-18 15:57:32 -0700

    cleaned up boxes

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,8 +1,8 @@
 var startLat
 var startLong
 var makeMarker = {}
-var markerToMake={}
-var venueForMarkers=[]
+var markerToMake = {}
+var venueForMarkers = []
 var name1
 var name2
 var name3
@@ -12,7 +12,7 @@ var latLon
 function cards() {
 
   for (let i = 0; i < eventsFromUserChoices[0].length; i++) {
-    
+
     $(".main-container>.row").append(` <div class="col s2 m7"> 
     
     <div class="card horizontal">
@@ -23,19 +23,15 @@ function cards() {
 
         <div id= "card-contents" class="card-content">
             <h5 id="artist-name">${eventsFromUserChoices[2][i]}</h5>
-            <h6 id="venue-name">Venue Name: ${eventsFromUserChoices[5][i]}   |  Date:  ${eventsFromUserChoices[0][i]}   |  Time:  ${eventsFromUserChoices[1][i]}</h6>
-            <p>I am a very simple card. I am good at containing small bits of information.</p>
+            <h6 id="venue-name">Event Info: |  Date: ${eventsFromUserChoices[0][i]} | ${eventsFromUserChoices[5][i]} | Time: ${eventsFromUserChoices[1][i]}</h6>
             </div> 
           
 
         <div class="card-action">
-        <a id="button-view" class="waves-effect waves-light btn venue-buttons"><i class="material-icons left">map</i>Location</a>
+        <a id="venue-name" class="waves-effect waves-light btn venue-buttons" data-venue="${(eventsFromUserChoices[5][i])}", data-lat="${eventsFromUserChoices[6][i]}", data-long="${eventsFromUserChoices[7][i]}" id="button-view"><i class="material-icons left">map</i>Location on Maps</a>
 
-            <a class="waves-effect waves-light btn"><i class="material-icons left">music_video</i>Youtube</a>
-            <button class="venue-buttons" data-venue="${(eventsFromUserChoices[5][i])}", data-lat="${eventsFromUserChoices[6][i]}", data-long="${eventsFromUserChoices[7][i]}" id="button-view">Venue Location</button>
-            <a href="${eventsFromUserChoices[8][i]}" id="itunes"></i>Button</a>
-
-            <a href="#" id="event-details">Email Event Details</a>
+            <a href="${eventsFromUserChoices[8][i]}"class="waves-effect waves-light btn"><i class="material-icons left">music_video</i>Youtube</a>
+      
             <a class="waves-effect waves-light btn"><i class="material-icons left">email</i>Notifications</a>
            
         </div>
@@ -55,7 +51,7 @@ function mapFor() {
   if (search === "seattle") {
     seattleLocation()
   }
- 
+
 }
 //end leahs code
 
@@ -63,9 +59,9 @@ function mapFor() {
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      
+
       lat = position.coords.latitude,
-      lng = position.coords.longitude
+        lng = position.coords.longitude
       var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: new google.maps.LatLng(lat, lng),
@@ -78,43 +74,46 @@ function getLocation() {
       console.log(latLon)
       usersLocationQuery(latLon)
     })
-   
+
   }
-  
+
 }
 
 //if we dont have the users location this runs the map over seattle and supplies those cordinates for the ajax pull
 function seattleLocation() {
-  var map = new google.maps.Map(document.getElementById('map'),{
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: new google.maps.LatLng(47.608013, -122.335167),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
   latLon = (47.608013 + "," + -122.335167)
-console.log(latLon)
+  console.log(latLon)
   // bandInfo(latLon)
   seattleQuery(latLon)
 }
 //this doesnt work currently
 
 //this doesnt work either, it is supposed to draw the longitude/latitude/venue name out of the venue button for placing a marker
-$(document).on("click", ".venue-buttons", function(event) {
+$(document).on("click", ".venue-buttons", function (event) {
   event.preventDefault();
-  var lats=$(this).attr("data-lat")
-  var lon=$(this).attr("data-long")
-   var ven=$(this).attr("data-venue")
-console.log(ven)
-lats2=JSON.parse(lats)
-long2=JSON.parse(lon)
+  var lats = $(this).attr("data-lat")
+  var lon = $(this).attr("data-long")
+  var ven = $(this).attr("data-venue")
+  console.log(ven)
+  lats2 = JSON.parse(lats)
+  long2 = JSON.parse(lon)
 
-ven2=JSON.stringify(ven)
-console.log("venue name" +ven2)
+  ven2 = JSON.stringify(ven)
+  console.log("venue name" + ven2)
 
-console.log("stringify latitude" +lats2);
-console.log("stringify longitude"+long2);
+  console.log("stringify latitude" + lats2);
+  console.log("stringify longitude" + long2);
 
-  markerToMake={lat: lats2, lng: long2}
-   console.log(markerToMake)
+  markerToMake = {
+    lat: lats2,
+    lng: long2
+  }
+  console.log(markerToMake)
   venueMarkers(markerToMake, ven)
 })
 
@@ -132,5 +131,3 @@ function venueMarkers() {
     title: ven2
   });
 }
-
-
