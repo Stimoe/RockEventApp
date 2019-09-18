78cd9e5d16d187923d34a6b46e17a6d6520c5b7c
Stimoe <stimoe3@gmail.com>
2019-09-18 09:34:59 -0700

    fixed the dates, they are working nowgit add .

--- javascript/email.js
+++ javascript/email.js
@@ -1,20 +1,4 @@
-markerToMake={}
-venueForMarkers=[]
-ven=""
-  
-  // function displayNew(){
-      
-  //   $("#buttons-view").empty();
-  //   for (var j = 0; j < eventsFromUserChoices[0].length; j++) {
-        
-  //     var a = $("<button>");
-  //     a.attr({"data-name1": eventsFromUserChoices[5][j]}) 
-  //     a.attr({"data-name2": eventsFromUserChoices[6][j]})
-  //     a.attr({"data-name3": eventsFromUserChoices[7][j]})
-  //    a.attr("class","venue-buttons")
-  //     a.text(eventsFromUserChoices[5][j]);
-  //     $("#buttons-view").append(a)
-  // }}
+
 
  
     
