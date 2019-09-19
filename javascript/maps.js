464de94b2affa4fe7f9c66f9a1392bc3479290e5
Stimoe <stimoe3@gmail.com>
2019-09-18 18:46:12 -0700

    made the map stop at the footer

--- javascript/maps.js
+++ javascript/maps.js
@@ -155,3 +155,35 @@ function venueMarkers() {
     title: ven2
   });
 }
+
+$(function() {
+  $.fn.scrollBottom = function() {
+      return $(document).height() - this.scrollTop() - this.height();
+  };
+
+  var $el = $('#map');
+  var $window = $(window);
+
+  $window.bind("scroll resize", function() {
+      var gap = $window.height() - $el.height() - 10;
+      var visibleFoot = 172 - $window.scrollBottom();
+      var scrollTop = $window.scrollTop()
+      
+      if(scrollTop < 172 + 10){
+          $el.css({
+              top: (172 - scrollTop) + "px",
+              bottom: "auto"
+          });
+      }else if (visibleFoot > gap) {
+          $el.css({
+              top: "auto",
+              bottom: visibleFoot + "px"
+          });
+      } else {
+          $el.css({
+              top: 0,
+              bottom: "auto"
+          });
+      }
+  });
+});
